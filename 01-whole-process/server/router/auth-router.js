9784f6dfa120f94191fcd927acf89db85a8c3e1f
Shrikant Jamale <shrikantj0794@gmail.com>
2024-03-21 10:09:15 +0530

    create route of contact form

--- 01-whole-process/server/router/auth-router.js
+++ 01-whole-process/server/router/auth-router.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authcontroller = require('../controllers/auth-controller')
-//import validater part
-const schema = require('../validator/auth-validator')
-const validate = require('../middleware/validate-middleware')
 
 router.route("/register").post(authcontroller.register);
 router.route("/login").post(authcontroller.login)
